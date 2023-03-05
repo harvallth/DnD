a67b95f8fedeac52ef49d38cde051eb3382a2f15
Harvallth <sharp89@tlen.pl>
2023-03-05 11:20:31 +0100

    spells min/max dmg values

--- src/main/java/kt/dnd/magic/Spell.java
+++ src/main/java/kt/dnd/magic/Spell.java
@@ -1,12 +1,12 @@
 package kt.dnd.magic;
 
 public enum Spell {
-    BLIND(SpellSchool.ARCANE, 1, 3, 0),
-    CAT_FORM(SpellSchool.NATURE, 1, 8, 0),
-    CHARM(SpellSchool.ARCANE, 1, 2, 0),
-    FIREBALL(SpellSchool.FIRE, 1, 3, 2*8),
-    SPARKS(SpellSchool.AIR, 1, 3, 2*6),
-    REGROWTH(SpellSchool.NATURE, 1, 2, 0);
+    BLIND(SpellSchool.ARCANE, 1, 3, 0, 0),
+    CAT_FORM(SpellSchool.NATURE, 1, 8, 0, 0),
+    CHARM(SpellSchool.ARCANE, 1, 2, 0, 0),
+    FIREBALL(SpellSchool.FIRE, 1, 3, 2*1, 2*8),
+    SPARKS(SpellSchool.AIR, 1, 3, 2*1, 2*6),
+    REGROWTH(SpellSchool.NATURE, 1, 2, -2*1, -2*10);
 
-    Spell(SpellSchool school, int level, int manaCost, int damage) {}
+    Spell(SpellSchool school, int level, int manaCost, int minDamage, int maxDamage) {}
 }
